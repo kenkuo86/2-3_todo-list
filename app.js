7c2018af090d235d5f884a14d0668c34fd0e7e76
kenkuo86 <kenkuo860129@gmail.com>
2022-07-27 01:44:03 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express')
+// 引入 mongoose
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
 
+// 透過 mongoose 把應用程式 server 跟資料庫 server 進行連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+// 處理 db 連線失敗時的狀況
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 處理 db 連線成功時的狀況
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world')
---
+++ models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// 運用 mongoose 提供的 Schema 方法建立 todo list 資料的 schema -> 定義「一筆資料」的格式
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  done: {
+    type: Boolean
+  }
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
