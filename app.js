44ba3d3f0cccf42be507bf3e0bab00caf0c9cac4
kenkuo86 <kenkuo860129@gmail.com>
2022-07-29 01:49:45 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
-    .then(todos => { res.render('index', {todos} ) } )
+    .then( todos => { res.render('index', {todos} ) } )
     .catch( error => console.log(error) )  
 })
 
@@ -63,6 +63,27 @@ app.post('/todos', (req,res) => {
   // return Todo.create({ name })    
   //   .then(() => res.redirect('/'))
   //   .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch((error) => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+
+  return Todo.findById(id)
+    .then( (todo) => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => {res.redirect(`/todos/${id}`)})
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
