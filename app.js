3a3af679b5a42ad403e8ffaac4f7a557d016989c
kenkuo86 <kenkuo860129@gmail.com>
2022-08-05 01:39:14 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then( todos => { res.render('index', {todos} ) } )
     .catch( error => console.log(error) )  
 })
